e78d103342bce0a356864ba5a4f1b32dfe20bf48
QuqqU <rldnd913@naver.com>
2018-07-13 00:20:11 +0900

    modify test examples

--- Example/SplayDict/ViewController.swift
+++ Example/SplayDict/ViewController.swift
@@ -14,23 +14,31 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+       
         let c = [(2, 2), (6,4)]
         var a = SplayDict<Int, Int>(c)
         a[3] = 5
+        
+       
         a[4] = 7
         a.insert(key: 5, value: 6)
         a[3] = 10
         
+        print(a)
         
+        for d in a {
+            print(d)
+        }
       
         
         
-        for c in a {
-            print(c)
-        }
+        let e: SplayDict<Int, Int> = [Element(1,1), Element(2,4)]
         
-       
+        
+        print(e)
+        
+        
+        
     }
 
     
